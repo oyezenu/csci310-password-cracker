1a440cac2fde373324d3cb4a5c5b2d98744a0d0b
oyezenu <cyberzenu@gmail.com>
2026-04-07 09:36:33 -0500

    Added iterated hashing to generator

--- hash_generator.py
+++ hash_generator.py
@@ -3,14 +3,15 @@ import os
 
 passwords = ["password123", "hello", "letmein"]
 
-with open("salted_hashes.txt", "w") as f:
+with open("iterated_hashes.txt", "w") as f:
 	for password in passwords:
 	   salt = os.urandom(16)
-	   salted_password = salt + password.encode()
-	   hashed = hashlib.sha256(salted_password).hexdigest()
+	   hashed = hashlib.sha256(salt + password.encode()).hexdigest()
+	   for i in range(10000):
+	      hashed = hashlib.sha256(hashed.encode()).hexdigest()
 	   salt_hex = salt.hex()
 	   f.write(salt_hex + ":" + hashed + "\n")
 
-print("Salted hashes saved to salted_hashes.txt")
+print("Salted hashes saved to iterated_hashes.txt")
 
 
